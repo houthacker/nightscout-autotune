ee2fbb70f02eb09aa250fa9e1193536b72f5d817
houtacker <houthacker@houthacker.io>
2025-09-26 13:26:08 +0200

    Rename to nightscout-autotune

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM node:24-alpine
 WORKDIR /app
 COPY . .
 RUN npm install -g
-CMD ["/bin/sh", "-c", "nightscout-profile-converter -n $(echo $NS_HOST) -p $(echo $NS_PROFILE) -c $(echo $MIN_5MIN_CARBIMPACT) --autosens-min $(echo $AUTOSENS_MIN) --autosens-max $(echo $AUTOSENS_MAX) " ]
+CMD ["/bin/sh", "-c", "nightscout-autotune -n $(echo $NS_HOST) -p $(echo $NS_PROFILE) -c $(echo $MIN_5MIN_CARBIMPACT) --autosens-min $(echo $AUTOSENS_MIN) --autosens-max $(echo $AUTOSENS_MAX) " ]
--- src/fetch_profile.js
+++ src/fetch_profile.js
@@ -1,6 +1,14 @@
 import { URL } from 'node:url';
 import fetch from 'node-fetch';
 
+/**
+ * Fetches the requested profile from the Nightscout instance at `ns_url`. 
+ * 
+ * @param {string} ns_url The base url of the Nightscout instance.
+ * @param {string} profile_name The name of the profile to fetch, or `undefined` for the default profile.
+ * @returns The requested Nightscout profile.
+ * @throws `Error` - If no profiles exist or if the specific profile cannot be found.
+ */
 export async function fetch_profile(ns_url, profile_name = undefined) {
     let url = new URL("api/v2/profile.json", ns_url);
     let response = await fetch(url);
